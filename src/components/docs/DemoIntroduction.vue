133a8688dc4d4b7eac08deaa0195cbce31fc76a1
James Chim <jc.app.production@gmail.com>
2018-05-20 23:23:27 +1000

    Revised Doc

--- src/components/docs/DemoIntroduction.vue
+++ src/components/docs/DemoIntroduction.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1 class="my-3">Documentation</h1>
-    <p>Vue date slider library that is built for https://timehunter.io . It aims to provide an easy and user friendly date filter that can navigate back and forth by a simple click of a button. It is responsive and built with moment.</p>
+    <p>Vue date slider library that is built for <a href="https://timehunter.io" target="_blank">Time Hunter</a>. It aims to provide an easy and user friendly date filter that can navigate back and forth by a simple click of a button. It is responsive and built with moment.</p>
     <p>You can find its repository <a href="https://github.com/james-chim/vue-date-quick-slider" target="_blank">here</a>.</p>
   </div>
 </template>
